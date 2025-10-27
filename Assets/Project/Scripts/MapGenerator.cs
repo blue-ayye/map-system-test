c28c2217fdcc5483dafedc6c1a1ba7c9d1aa7903
blue-aye <ccoolbuddy4@gmail.com>
2025-10-28 03:10:47 +0530

    Select starting nodes

--- Assets/Project/Scripts/MapGenerator.cs
+++ Assets/Project/Scripts/MapGenerator.cs
@@ -1,12 +1,13 @@
-using System.Net.NetworkInformation;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace BP.MapGeneration
 {
+    [System.Serializable]
     public class MapNode
     {
-        public int Level { get; private set; }
-        public int NodeIndex { get; private set; }
+        public int Level;
+        public int NodeIndex;
 
         public MapNode(int level, int nodeIndex)
         {
@@ -20,22 +21,25 @@ namespace BP.MapGeneration
         [SerializeField] private int MaxLevels = 5;
         [SerializeField] private int NodesPerLevel = 8;
         [SerializeField] private int _playerInputSeed = 12345;
-        [SerializeField]private bool _usePlayerInputSeed = false;
+        [SerializeField] private bool _usePlayerInputSeed = false;
+        [SerializeField] private int _uniquePaths = 3;
+        [SerializeField] private int _totalPaths = 7;
 
         private MapNode[,] _mapGrid;
         private System.Random _pRNG;
         public int GeneratedSeed { get; private set; }
+        [SerializeField] private List<MapNode> _startingNodes = new List<MapNode>();
 
         private void OnDrawGizmos()
         {
             if (_mapGrid == null)
                 return;
-            Gizmos.color = Color.green;
             for (int level = 0; level < MaxLevels; level++)
             {
                 for (int nodeIndex = 0; nodeIndex < NodesPerLevel; nodeIndex++)
                 {
-                    Vector3 position = new Vector3(nodeIndex * 2.0f, level * 2.0f,0);
+                    Gizmos.color = _startingNodes.Contains(_mapGrid[level, nodeIndex]) ? Color.green : Color.white;
+                    Vector3 position = new Vector3(nodeIndex * 2.0f, level * 2.0f, 0);
                     Gizmos.DrawSphere(position, 0.3f);
                 }
             }
@@ -43,9 +47,10 @@ namespace BP.MapGeneration
 
         private void Start()
         {
-            CreateNodeGrid();
             int? seed = _usePlayerInputSeed ? _playerInputSeed : (int?)null;
             InitializePRNG(seed);
+            CreateNodeGrid();
+            SelectStartingNodes();
         }
 
         private void CreateNodeGrid()
@@ -75,6 +80,31 @@ namespace BP.MapGeneration
             }
 
             _pRNG = new System.Random(GeneratedSeed);
+        }
+
+        private void SelectStartingNodes()
+        {
+            int startingLevel = 0;
+            _startingNodes.Clear();
+            // Ensure at least <MinUniqueStartingPoints> number of nodes are unique
+            for (int i = 0; i < _uniquePaths; i++)
+            {
+                MapNode randomNode;
+                do
+                {
+                    int randomIndex = _pRNG.Next(0, NodesPerLevel);
+                    randomNode = _mapGrid[startingLevel, randomIndex];
+                } while (_startingNodes.Contains(randomNode));
+                _startingNodes.Add(randomNode);
+            }
+
+            // Then fill the rest allowing duplicate nodes to allow multiple paths from same starting point
+            while (_startingNodes.Count < _totalPaths)
+            {
+                int randomIndex = _pRNG.Next(0, NodesPerLevel);
+                var randomNode = _mapGrid[startingLevel, randomIndex];
+                _startingNodes.Add(randomNode);
+            }
         }
     }
 }
