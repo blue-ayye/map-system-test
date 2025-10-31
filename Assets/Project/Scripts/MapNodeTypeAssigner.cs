42e552c94e6e3056a7d934ba468b967bd1700504
blue-aye <ccoolbuddy4@gmail.com>
2025-10-31 19:30:35 +0530

    Split MapNodeTypeAssigner from the main script

---
+++ Assets/Project/Scripts/MapNodeTypeAssigner.cs
@@ -0,0 +1,158 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BP.MapSystem
+{
+    public class MapNodeTypeAssigner : MonoBehaviour
+    {
+        [SerializeField] private List<NodeTypeRulesSO> _nodeTypeRules;
+        [SerializeField] private MapNodeTypeSO _defaultNodeType;
+
+        public void AssignNodeTypes(MapNode[,] mapGrid, System.Random pRNG)
+        {
+            int maxLevels = mapGrid.GetLength(0);
+
+            // 1. First, process rules that are marked to exclude from other rules
+            var staticLevels = _nodeTypeRules.Where(rules => rules.ExcludeFromOtherRules).ToList();
+            SetNodeTypeByRules(mapGrid, pRNG, staticLevels);
+
+            // 2. Then, process the remaining rules so static levels influence them
+            var normalLevels = _nodeTypeRules.Where(rules => !rules.ExcludeFromOtherRules).ToList();
+            SetNodeTypeByRules(mapGrid, pRNG, normalLevels);
+        }
+
+        private void SetNodeTypeByRules(MapNode[,] mapGrid, System.Random pRNG, List<NodeTypeRulesSO> nodeTypeRules)
+        {
+            int nodesPerLevel = mapGrid.GetLength(1);
+
+            foreach (var rule in nodeTypeRules)
+            {
+                for (int level = rule.StartLevel; level <= rule.EndLevel; level++)
+                {
+                    for (int nodeIndex = 0; nodeIndex < nodesPerLevel; nodeIndex++)
+                    {
+                        var node = mapGrid[level, nodeIndex];
+                        if (node == null) continue;
+
+                        var nodeType = GetValidNodeType(node, rule, pRNG);
+                        node.NodeType = nodeType;
+
+                        if (node.NodeView != null)
+                            node.NodeView.SetNodeType(nodeType);
+                    }
+                }
+            }
+        }
+
+        private MapNodeTypeSO GetValidNodeType(MapNode currentNode, NodeTypeRulesSO nodeTypeRules, System.Random pRNG)
+        {
+            if (nodeTypeRules.NodeTypeWeights.Count == 0)
+            {
+                Debug.LogWarning($"NodeTypeRulesSO '{nodeTypeRules.name}' has no NodeTypeWeights defined. Assigning default node type.");
+                return _defaultNodeType;
+            }
+
+            // If excluded from other rules, use weights as-is
+            if (nodeTypeRules.ExcludeFromOtherRules)
+            {
+                return GetNodeTypeByWeight(currentNode, nodeTypeRules.NodeTypeWeights, pRNG);
+            }
+
+            // Make a copy of the weights to modify
+            Dictionary<MapNodeTypeSO, float> availableWeights = new Dictionary<MapNodeTypeSO, float>(nodeTypeRules.NodeTypeWeights);
+
+            // Reduce weights of consecutive node types
+            ApplyConsecutiveRules(currentNode, nodeTypeRules, availableWeights);
+
+            // Use Seed: 817167126 Grid: 9x7 Path: 3/7 to demonstrate
+            ApplySiblingConstraintRules(currentNode, nodeTypeRules, availableWeights);
+
+            return GetNodeTypeByWeight(currentNode, availableWeights, pRNG);
+        }
+
+        private static void ApplySiblingConstraintRules(MapNode currentNode, NodeTypeRulesSO nodeTypeRules, Dictionary<MapNodeTypeSO, float> availableWeights)
+        {
+            if (nodeTypeRules.SiblingTypeConstraint == SiblingNodeTypeConstraint.AllowSameType)
+                return;
+
+            var allSiblings = currentNode.ParentNodes.SelectMany(p => p.ChildNodes).Where(c => c != currentNode).Distinct().ToList();
+            MapNode previousSibling = null;
+            MapNode nextSibling = null;
+            foreach (var sibling in allSiblings)
+            {
+                if (sibling.Index < currentNode.Index)
+                {
+                    if (previousSibling == null || sibling.Index > previousSibling.Index)
+                    {
+                        previousSibling = sibling;
+                    }
+                }
+                else if (sibling.Index > currentNode.Index)
+                {
+                    if (nextSibling == null || sibling.Index < nextSibling.Index)
+                    {
+                        nextSibling = sibling;
+                    }
+                }
+            }
+
+            var siblingsToCheck = nodeTypeRules.SiblingTypeConstraint == SiblingNodeTypeConstraint.DisallowSameTypeForImmediateSiblings
+                ? new List<MapNode> { previousSibling, nextSibling }
+                : allSiblings;
+
+            foreach (var sibling in siblingsToCheck)
+            {
+                if (sibling != null && sibling.NodeType != null && availableWeights.ContainsKey(sibling.NodeType))
+                {
+                    availableWeights.Remove(sibling.NodeType);
+                }
+            }
+        }
+
+        private static void ApplyConsecutiveRules(MapNode currentNode, NodeTypeRulesSO nodeTypeRules, Dictionary<MapNodeTypeSO, float> availableWeights)
+        {
+            var consecutiveNodes = new List<MapNode>(currentNode.ParentNodes).Concat(currentNode.ChildNodes).Where(cn => cn.NodeType != null).ToList();
+            foreach (var consecutiveNode in consecutiveNodes)
+            {
+                if (nodeTypeRules.ConsecutiveTypeWeightReductions.TryGetValue(consecutiveNode.NodeType, out float reductionValue) && availableWeights.ContainsKey(consecutiveNode.NodeType))
+                {
+                    float newValue = availableWeights[consecutiveNode.NodeType] - reductionValue;
+                    if (newValue <= 0f)
+                    {
+                        availableWeights.Remove(consecutiveNode.NodeType);
+                    }
+                    else
+                    {
+                        availableWeights[consecutiveNode.NodeType] = newValue;
+                    }
+                }
+            }
+        }
+
+        private MapNodeTypeSO GetNodeTypeByWeight(MapNode node, Dictionary<MapNodeTypeSO, float> availableWeights, System.Random pRNG)
+        {
+            float totalWeight = availableWeights.Values.Sum();
+            if (totalWeight <= 0f)
+            {
+                Debug.LogWarning($"All node types have zero weight for node at Level {node.Level}, Index {node.Index}. Assigning default node type.");
+                return _defaultNodeType;
+            }
+            float randomValue = (float)(pRNG.NextDouble() * totalWeight);
+            float cumulativeWeight = 0f;
+
+            for (int i = 0; i < availableWeights.Count; i++)
+            {
+                var kvp = availableWeights.ElementAt(i);
+                cumulativeWeight += kvp.Value;
+                if (randomValue <= cumulativeWeight)
+                {
+                    return kvp.Key;
+                }
+            }
+
+            Debug.LogWarning($"Failed to assign a node type for node at Level {node.Level}, Index {node.Index}. Assigning default node type.");
+            return _defaultNodeType; // Fallback in case of rounding errors
+        }
+    }
+}
--- Assets/Project/Scripts/MapSystemManager.cs
+++ Assets/Project/Scripts/MapSystemManager.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace BP.MapSystem
@@ -8,6 +6,7 @@ namespace BP.MapSystem
     {
         [SerializeField] private MapGridGenerator _mapGridGenerator;
         [SerializeField] private MapPathGenerator _mapPathGenerator;
+        [SerializeField] private MapNodeTypeAssigner _mapNodeTypeAssigner;
         [SerializeField] private int _playerInputSeed = 0;
         [SerializeField] private bool _usePlayerInputSeed = false;
 
@@ -36,7 +35,7 @@ namespace BP.MapSystem
             _mapGridGenerator.CreateNodeViews();
             _mapPathGenerator.CreatePathViews();
 
-            AssignNodeTypes(mapGrid, pRNG);
+            _mapNodeTypeAssigner.AssignNodeTypes(mapGrid, pRNG);
         }
 
         private System.Random InitializePRNG(int? seed)
@@ -53,152 +52,6 @@ namespace BP.MapSystem
             }
 
             return new System.Random(GeneratedSeed);
-        }
-
-        [SerializeField] private List<NodeTypeRulesSO> _nodeTypeRules;
-        [SerializeField] private MapNodeTypeSO _defaultNodeType;
-
-        private void AssignNodeTypes(MapNode[,] mapGrid, System.Random pRNG)
-        {
-            int maxLevels = mapGrid.GetLength(0);
-            int nodesPerLevel = mapGrid.GetLength(1);
-
-            var excludeFromOtherRulesLevels = _nodeTypeRules.Where(rules => rules.ExcludeFromOtherRules).ToList();
-            foreach (var rule in excludeFromOtherRulesLevels)
-            {
-                for (int level = rule.StartLevel; level <= rule.EndLevel; level++)
-                {
-                    for (int nodeIndex = 0; nodeIndex < nodesPerLevel; nodeIndex++)
-                    {
-                        var node = mapGrid[level, nodeIndex];
-                        if (node != null)
-                        {
-                            var nodeType = GetValidNodeType(node, rule, pRNG);
-                            node.NodeType = nodeType;
-                            var nodeView = node.NodeView;
-                            if (nodeView != null)
-                            {
-                                nodeView.SetNodeType(nodeType);
-                            }
-                        }
-                    }
-                }
-            }
-
-            for (int level = 0; level < maxLevels; level++)
-            {
-                NodeTypeRulesSO nodeTypeRules = _nodeTypeRules.Find(rules => level >= rules.StartLevel && level <= rules.EndLevel);
-                if (nodeTypeRules == null) continue;
-
-                for (int nodeIndex = 0; nodeIndex < nodesPerLevel; nodeIndex++)
-                {
-                    var node = mapGrid[level, nodeIndex];
-                    if (node != null)
-                    {
-                        var nodeType = GetValidNodeType(node, nodeTypeRules, pRNG);
-                        node.NodeType = nodeType;
-                        var nodeView = node.NodeView;
-                        if (nodeView != null)
-                        {
-                            nodeView.SetNodeType(nodeType);
-                        }
-                    }
-                }
-            }
-        }
-
-        private MapNodeTypeSO GetValidNodeType(MapNode currentNode, NodeTypeRulesSO nodeTypeRules, System.Random pRNG)
-        {
-            if (nodeTypeRules.NodeTypeWeights.Count == 0)
-            {
-                Debug.LogWarning($"NodeTypeRulesSO '{nodeTypeRules.name}' has no NodeTypeWeights defined. Assigning default node type.");
-                return _defaultNodeType;
-            }
-
-            // Make a copy of the weights to modify
-            Dictionary<MapNodeTypeSO, float> availableWeights = new Dictionary<MapNodeTypeSO, float>(nodeTypeRules.NodeTypeWeights);
-
-            // Reduce weights of consecutive node types
-            var consecutiveNodes = new List<MapNode>(currentNode.ParentNodes).Concat(currentNode.ChildNodes).Where(cn => cn.NodeType != null).ToList();
-            foreach (var consecutiveNode in consecutiveNodes)
-            {
-                if (nodeTypeRules.ConsecutiveTypeWeightReductions.TryGetValue(consecutiveNode.NodeType, out float reductionValue) && availableWeights.ContainsKey(consecutiveNode.NodeType))
-                {
-                    float newValue = availableWeights[consecutiveNode.NodeType] - reductionValue;
-                    if (newValue <= 0f)
-                    {
-                        availableWeights.Remove(consecutiveNode.NodeType);
-                    }
-                    else
-                    {
-                        availableWeights[consecutiveNode.NodeType] = newValue;
-                    }
-                }
-            }
-
-            // Apply sibling node type constraints (Use Seed: 817167126 Grid: 9x7 Path: 3/7 to demonstrate)
-            if (nodeTypeRules.SiblingTypeConstraint != SiblingNodeTypeConstraint.AllowSameType)
-            {
-                var allSiblings = currentNode.ParentNodes.SelectMany(p => p.ChildNodes).Where(c => c != currentNode).Distinct().ToList();
-                MapNode previousSibling = null;
-                MapNode nextSibling = null;
-                foreach (var sibling in allSiblings)
-                {
-                    if (sibling.Index < currentNode.Index)
-                    {
-                        if (previousSibling == null || sibling.Index > previousSibling.Index)
-                        {
-                            previousSibling = sibling;
-                        }
-                    }
-                    else if (sibling.Index > currentNode.Index)
-                    {
-                        if (nextSibling == null || sibling.Index < nextSibling.Index)
-                        {
-                            nextSibling = sibling;
-                        }
-                    }
-                }
-
-                var siblingsToCheck = nodeTypeRules.SiblingTypeConstraint == SiblingNodeTypeConstraint.DisallowSameTypeForImmediateSiblings
-                    ? new List<MapNode> { previousSibling, nextSibling }
-                    : allSiblings;
-
-                foreach (var sibling in siblingsToCheck)
-                {
-                    if (sibling != null && sibling.NodeType != null && availableWeights.ContainsKey(sibling.NodeType))
-                    {
-                        availableWeights.Remove(sibling.NodeType);
-                    }
-                }
-            }
-
-            return GetNodeTypeByWeight(currentNode, availableWeights, pRNG);
-        }
-
-        private MapNodeTypeSO GetNodeTypeByWeight(MapNode node, Dictionary<MapNodeTypeSO, float> availableWeights, System.Random pRNG)
-        {
-            float totalWeight = availableWeights.Values.Sum();
-            if (totalWeight <= 0f)
-            {
-                Debug.LogWarning($"All node types have zero weight for node at Level {node.Level}, Index {node.Index}. Assigning default node type.");
-                return _defaultNodeType;
-            }
-            float randomValue = (float)(pRNG.NextDouble() * totalWeight);
-            float cumulativeWeight = 0f;
-
-            for (int i = 0; i < availableWeights.Count; i++)
-            {
-                var kvp = availableWeights.ElementAt(i);
-                cumulativeWeight += kvp.Value;
-                if (randomValue <= cumulativeWeight)
-                {
-                    return kvp.Key;
-                }
-            }
-
-            Debug.LogWarning($"Failed to assign a node type for node at Level {node.Level}, Index {node.Index}. Assigning default node type.");
-            return _defaultNodeType; // Fallback in case of rounding errors
         }
     }
 }
